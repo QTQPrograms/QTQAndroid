c2682e2fd03af5f0d7fefa41644876fbe52d5db8
luanhui <1029149336@qq.com>
2016-01-19 17:09:37 +0800

    添加tab

--- app/src/main/java/com/qtq/qtqandroid/LoginActivity.java
+++ app/src/main/java/com/qtq/qtqandroid/LoginActivity.java
@@ -3,6 +3,7 @@ package com.qtq.qtqandroid;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -66,6 +67,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button btnMain=(Button)findViewById(R.id.login_btnMain);
+        btnMain.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(LoginActivity.this,TabMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
